4c6281b7bed712690e7e4cad3021b8d5e274fc26
Tomasz Krawczyk <grubach1@gmail.com>
2025-04-25 19:04:22 +0200

    Add cover image to Player component; utilize song video URL for dynamic loading.

--- src/components/controls/Player.tsx
+++ src/components/controls/Player.tsx
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
 import { loadPlayer } from "../../utils/player";
 import style from "./Player.module.css";
+import useStoreState from "../../hooks/useStoreState";
+import { appStore } from "../../utils/app";
 
 const Player = () => {
+  const { song } = useStoreState(appStore);
+
   useEffect(() => {
     loadPlayer();
   }, []);
@@ -12,6 +16,11 @@ const Player = () => {
       <div className={style.video}>
         <div className={style.iframe} id="video-player"></div>
       </div>
+      <img
+        className={style.cover}
+        src={`https://img.youtube.com/vi/${song.video}/0.jpg`}
+        alt={song.title}
+      />
     </div>
   );
 };
