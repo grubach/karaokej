684f19810e5e4188fc016434b4500fcb11ed5030
Tomasz Krawczyk <grubach1@gmail.com>
2025-04-26 20:03:18 +0200

    Reorder video assignment in parseSongScored function to prioritize original video over karaoke video

---
+++ src/songs/summer-wine.ts
@@ -0,0 +1,59 @@
+/*
+Strawberries, cherries and an angel's kiss in spring
+My summer wine is really made from all these things
+I walked in town on silver spurs that jingled to
+A song that I had only sang to just a few
+She saw my silver spurs and said let's pass some time
+And I will give to you summer wine
+Oh, summer wine
+Strawberries, cherries and an angel's kiss in spring
+My summer wine is really made from all these things
+Take off your silver spurs and help me pass the time
+And I will give to you summer wine
+Oh, summer wine
+My eyes grew heavy and my lips, they could not speak
+I tried to get up but I couldn't find my feet
+She reassured me with an unfamiliar line
+And then she gave to me more summer wine
+Oh, summer wine
+Strawberries, cherries and an angel's kiss in spring
+My summer wine is really made from all these things
+Take off your silver spurs and help me pass the time
+And I will give to you summer wine
+Summer wine
+When I woke up the sun was shining in my eyes
+My silver spurs were gone, my head felt twice it's size
+She took my silver spurs, a dollar and a dime
+And left me cravin' for more summer wine
+Oh, summer wine
+Strawberries, cherries and an angel's kiss in spring
+My summer wine is really made from all these things
+Take off those silver spurs and help me pass the time
+And I will give to you my summer wine
+Oh, summer wine
+*/
+
+export default {
+  id: "summer-wine",
+  title: "Summer Wine",
+  artist: "Nancy Sinatra & Lee Hazlewood",
+  bpm: 146,
+  startTime: 0.23,
+  endTime: 2 * 60 + 57.4,
+  karaokeVideo: "fJYq2L4LfAY",
+  originalVideo: "IVe_DsEi4pY",
+  score: `
+    P:2
+  
+    A4:1.5:Straw- A4:0.5:ber- B4:1.5:ries P:1 C5:0.5:cher- E5:2.5:ries D5:2:and C5:0.5:an D5:0.5:an- D5:1.5:gels D5:0.5:kiss D5:1:in D5:3:spring P:3
+    A4:1:My A4:0.5:sum- B4:0.5:mer C5:1:wine P:1.5 E5:1:is D5:0.5:real- C5:1:ly G5:1.5:made G5:0.5:from G5:1:all G5:1.5:these G5:5:things 
+    P:18.5
+
+    A4:1:I A4:1:walked B4:1:in C5:1:town E5:1:on D5:1:sil- C5:1:ver D5:1.5:spurs D5:0.5:that D5:0.5:jin- D5:1:gled D5:1:to P:4
+    A4:1:A A4:1:song B4:1:that C5:1.5:I E5:1:had D5:1:on- C5:1:ly G5:1.5:sang G5:0.5:to G5:0.5:just G5:1:a G5:0.5:few P:4.5
+    F5:1:She F5:1:saw G5:1:my A5:1:sil- G5:1:ver F5:1.5:spurs D5:0.5:and E5:1.5:said D5:0.5:let's C5:1.5:pass D5:0.5:some E5:1.5:time P:4
+    D5:0.5:And D5:1.5:I E5:1:will F5:1:give E5:1:to D5:2:you P:2 A4:0.5:sum- G4:1.5:mer A4:2:wine P:3
+    D5:4:Oh, C5:2 B4:1:sum- G4:1:mer A4:2:wine
+
+  `,
+};
