74d843e7cfb306e650cfa989e78643c47ca492e5
Tomasz Krawczyk <grubach1@gmail.com>
2025-05-04 08:58:51 +0200

    Poprawa dokładności tekstu piosenki 'Illusion' poprzez korektę błędów w pisowni

--- src/songs/illusion.ts
+++ src/songs/illusion.ts
@@ -61,10 +61,10 @@ C#5:0.25:Think:narrow C#5:0.25:you C#5:0.5:play G#4:0.5:your G#4:0.5:cards D#4:1
 P:1.5 
 G#4:0.5:Don't G#4:0.5:you F#4:0.5:know G#4:0.5:I F#4:0.5:can G#4:0.5:do F#4:0.5:this G#4:1:dance G#4:1:all G4:1:night?
 P:1.25 
-A#4:1:Ooh, D#4:1 P:1 F#4:0.5:what D#4:0.5:you F#4:1:Do- D#4:0.5 G#4:0.5:in'? D#4:0.5 P:1.5
+A#4:1:Ooh, D#4:1 P:1 F#4:0.5:what D#4:0.5:you F#4:1:do- D#4:0.5 G#4:0.5:in'? D#4:0.5 P:1.5
 D#4:0.5:Don't D#4:0.5:know B4:0.5:who B4:0.5:you A#4:0.5:think A#4:0.5:that G#4:0.5:you're G#4:0.5:con- F#4:1:fu- D#4:0.5 F#4:1:sin' 
 D#4:0.5:I D#4:0.5:be D#4:0.5:like,
-A#4:1:ooh, D#4:1 P:1 F#4:0.5:it's D#4:0.5:a- F#4:1:mu- D#4:0.5 G#4:0.5:sin'? D#4:0.5 P:1 
+A#4:1:ooh, D#4:1 P:1 F#4:0.5:it's D#4:0.5:a- F#4:1:mu- D#4:0.5 G#4:0.5:sin' D#4:0.5 P:1 
 D#4:0.5:You D#4:0.5:think D#4:0.5:I'm B4:0.5:gon- B4:0.5:na A#4:0.5:fall A#4:0.5:for G#4:0.5:an G#4:0.5:il- 
 F#4:0.5:lu- D#4:0.5 F#4:0.5 D#4:0.5 F#4:0.5 D#4:0.5 F#4:0.5 D#4:0.5 F#4:0.5 D#4:0.5 F#4:0.5 D#4:0.5 F#4:0.5 D#4:1 C#4:2:sion 
 P:27.5
@@ -73,7 +73,7 @@ C#5:0.5:Was C#5:0.5:a D#5:1:time A#4:0.5:when G#4:0.5:that F#4:0.5:shit G#4:1:mi
 C#5:0.5:Was C#5:0.5:a D#5:1:time A#4:0.5:when G#4:0.5:I F#4:0.5:just G#4:0.5:threw G#4:0.5:a G#4:0.5:match F#4:0.5:and G#4:0.5:let F#4:0.5:it A#4:1:burn P:0.5 
 C#5:0.5:Now C#5:0.5:I'm D#5:1:grown, A#4:0.5:I G#4:0.5:know F#4:0.5:what G#4:0.5:I G#4:0.5:de- A#4:1.5:serve P:1 
 
-G#4:0.5:I A#4:1:still F#5:0.5:Like F5:1:dan- A#4:0.5:cing G#4:0.5:with F#4:0.5:the G#4:0.5:les- G#4:0.5:sons G#4:0.5:I F#4:0.5:all- G#4:0.5:rea- F#4:0.5:dy A#4:1:learned
+G#4:0.5:I A#4:1:still F#5:0.5:like F5:1:dan- A#4:0.5:cing G#4:0.5:with F#4:0.5:the G#4:0.5:les- G#4:0.5:sons G#4:0.5:I F#4:0.5:all- G#4:0.5:rea- F#4:0.5:dy A#4:1:learned
 
 P:1.25
 
@@ -88,16 +88,16 @@ C#5:0.25:Think:narrow C#5:0.25:you C#5:0.5:play G#4:0.5:your G#4:0.5:cards D#4:1
 P:1.5 
 G#4:0.5:Don't G#4:0.5:you F#4:0.5:know G#4:0.5:I F#4:0.5:can G#4:0.5:do F#4:0.5:this G#4:1:dance G#4:1:all G4:1:night?
 P:1.25 
-A#4:1:Ooh, D#4:1 P:1 F#4:0.5:what D#4:0.5:you F#4:1:Do- D#4:0.5 G#4:0.5:in'? D#4:0.5 P:1.5
+A#4:1:Ooh, D#4:1 P:1 F#4:0.5:what D#4:0.5:you F#4:1:do- D#4:0.5 G#4:0.5:in'? D#4:0.5 P:1.5
 D#4:0.5:Don't D#4:0.5:know B4:0.5:who B4:0.5:you A#4:0.5:think A#4:0.5:that G#4:0.5:you're G#4:0.5:con- F#4:1:fu- D#4:0.5 F#4:1:sin' 
 D#4:0.5:I D#4:0.5:be D#4:0.5:like,
-A#4:1:ooh, D#4:1 P:1 F#4:0.5:it's D#4:0.5:a- F#4:1:mu- D#4:0.5 G#4:0.5:sin'? D#4:0.5 P:1 
+A#4:1:ooh, D#4:1 P:1 F#4:0.5:it's D#4:0.5:a- F#4:1:mu- D#4:0.5 G#4:0.5:sin' D#4:0.5 P:1 
 D#4:0.5:You D#4:0.5:think D#4:0.5:I'm B4:0.5:gon- B4:0.5:na A#4:0.5:fall A#4:0.5:for G#4:0.5:an G#4:0.5:il- 
 F#4:0.5:lu- D#4:0.5 F#4:0.5 D#4:0.5 F#4:0.5 D#4:0.5 F#4:0.5 D#4:0.5 F#4:0.5 D#4:0.5 F#4:0.5 D#4:0.5 F#4:0.5 D#4:1 C#4:2:sion 
 P:26.5
 
 E#4:0.5:Il- G#4:0.75:lu- A#4:0.25 G#4:0.5 F#4:1:sion P:1
-A#4:0.5:I G#4:0.5:real- F#4:0.5:ly D#4:0.5:like C#4:0.5:the D#4:0.5:way F#4:0.5:you're P:1 G#4:0.75:Mo- A#4:0.25 G#4:0.5 F#4:0.5:vin' P:1.5
+A#4:0.5:I G#4:0.5:real- F#4:0.5:ly D#4:0.5:like C#4:0.5:the D#4:0.5:way F#4:0.5:you're P:1 G#4:0.75:mo- A#4:0.25 G#4:0.5 F#4:0.5:vin' P:1.5
 A#4:0.5:Yeah G#4:0.5:I F#4:0.5:just D#4:0.5:wan- C#4:0.5:na D#4:0.5:dance F#4:0.5:with D#4:0.5:the F#4:0.5:il- G#4:0.75:lu- A#4:0.25 G#4:0.5 F#4:0.5:sion
 P:1.5
 A#4:0.5:Yeah G#4:0.5:I F#4:0.5:just D#4:0.5:wan- C#4:0.5:na D#4:0.5:dance F#4:0.5:with 
@@ -106,10 +106,10 @@ A#4:0.5:Yeah G#4:0.5:I F#4:0.5:just D#4:0.5:wan- C#4:0.5:na D#4:0.5:dance F#4:0.
 D#4:0.5:I F#4:0.5:could G#4:1:dance G#4:1:all G4:1:night 
 P:33.5
 
-A#4:1:Ooh, D#4:1 P:1 F#4:0.5:what D#4:0.5:you F#4:1:Do- D#4:0.5 G#4:0.5:in'? D#4:0.5 P:1.5
+A#4:1:Ooh, D#4:1 P:1 F#4:0.5:what D#4:0.5:you F#4:1:do- D#4:0.5 G#4:0.5:in'? D#4:0.5 P:1.5
 D#4:0.5:Don't D#4:0.5:know B4:0.5:who B4:0.5:you A#4:0.5:think A#4:0.5:that G#4:0.5:you're G#4:0.5:con- F#4:1:fu- D#4:0.5 F#4:1:sin' 
 D#4:0.5:I D#4:0.5:be D#4:0.5:like,
-A#4:1:ooh, D#4:1 P:1 F#4:0.5:it's D#4:0.5:a- F#4:1:mu- D#4:0.5 G#4:0.5:sin'? D#4:0.5 P:1 
+A#4:1:ooh, D#4:1 P:1 F#4:0.5:it's D#4:0.5:a- F#4:1:mu- D#4:0.5 G#4:0.5:sin' D#4:0.5 P:1 
 D#4:0.5:You D#4:0.5:think D#4:0.5:I'm B4:0.5:gon- B4:0.5:na A#4:0.5:fall A#4:0.5:for G#4:0.5:an G#4:0.5:il- 
 F#4:0.5:lu- D#4:0.5 F#4:0.5 D#4:0.5 A#4:0.5 G#4:0.5 F#4:0.5 G#4:0.5 
 
